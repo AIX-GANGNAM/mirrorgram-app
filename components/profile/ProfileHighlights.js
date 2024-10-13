9d5052e4b92e815c60c38d4f179bde642106b508
yoonjae <realyoon77@gmail.com>
2024-10-13 21:19:31 +0900

    하이라이트 수정

--- components/profile/ProfileHighlights.js
+++ components/profile/ProfileHighlights.js
@@ -4,11 +4,11 @@ import { Ionicons } from '@expo/vector-icons';
 
 const ProfileHighlights = () => {
   const highlights = [
-    { id: 1, title: '여행', image: 'https://via.placeholder.com/150' },
-    { id: 2, title: '음식', image: 'https://via.placeholder.com/150' },
-    { id: 3, title: '취미', image: 'https://via.placeholder.com/150' },
-    { id: 4, title: '일상', image: 'https://via.placeholder.com/150' },
-    { id: 5, title: '운동', image: 'https://via.placeholder.com/150' },
+    { id: 1, title: '기쁜놈', image: 'https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/gI8/image/nl4J4OCc7QyIoC8rBK8Fn1kYVCc.jpg' },
+    { id: 2, title: '화남놈', image: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201506/28/htm_20150628083828504.jpg' },
+    { id: 3, title: '까칠이', image: 'https://inabooth.io/_next/image?url=https%3A%2F%2Fd19bi7owzxc0m2.cloudfront.net%2Fprod%2Fcharacter_files%2F19dec92d-10be-4f5a-aad9-c68846c3d4b7.jpeg&w=3840&q=75' },
+    { id: 4, title: '슬픔', image: 'https://d3ihz389yobwks.cloudfront.net/1597427709625898634218810800.jpg' },
+    { id: 5, title: '선비', image: 'https://img.newspim.com/news/2017/01/31/1701311632536400.jpg' },
   ];
 
   return (
@@ -21,8 +21,10 @@ const ProfileHighlights = () => {
       </TouchableOpacity>
       {highlights.map((highlight) => (
         <TouchableOpacity key={highlight.id} style={styles.highlightItem}>
-          <Image source={{ uri: highlight.image }} style={styles.highlightImage} />
-          <Text style={styles.highlightText}>{highlight.title}</Text>
+          <View style={styles.highlightImageContainer}>
+            <Image source={{ uri: highlight.image }} style={styles.highlightImage} />
+          </View>
+          <Text style={styles.highlightText} numberOfLines={1}>{highlight.title}</Text>
         </TouchableOpacity>
       ))}
     </ScrollView>
@@ -31,33 +33,45 @@ const ProfileHighlights = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 10,
+    paddingVertical: 15,
+    backgroundColor: '#fff', // 배경색을 흰색으로 변경
   },
   highlightItem: {
     alignItems: 'center',
     marginRight: 15,
+    width: 70,
   },
   addButton: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: '#262626',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#f0f0f0', // 배경색을 밝은 회색으로 변경
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
-    borderColor: '#fff',
+    borderColor: '#dbdbdb', // 테두리 색상을 연한 회색으로 변경
+  },
+  highlightImageContainer: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#dbdbdb', // 테두리 색상을 연한 회색으로 변경
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
   },
   highlightImage: {
     width: 60,
     height: 60,
     borderRadius: 30,
-    borderWidth: 1,
-    borderColor: '#fff',
   },
   highlightText: {
-    color: '#fff',
+    color: '#262626', // 텍스트 색상을 어두운 회색으로 변경
     marginTop: 5,
     fontSize: 12,
+    textAlign: 'center',
+    width: '100%',
   },
 });
 
