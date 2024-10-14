1e311765e4397296784edd996903be28854e00bc
yoonjae <realyoon77@gmail.com>
2024-10-14 17:51:11 +0900

    이미지 백터db

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, ScrollView, SafeAreaView, View, Platform } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView, Platform } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, doc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { setUser } from '../store/slice/userSlice'; // setUser 액션 가져오기
 import Header from '../components/profile/editProfile/Header';
 import EditForm from '../components/profile/editProfile/EditForm';
+import * as ImagePicker from 'expo-image-picker';
 
 const EditProfileScreen = ({route, navigation}) => {
 	const dispatch = useDispatch();
 	const auth = getAuth();
 	const db = getFirestore();
+	const storage = getStorage();
 	const currentUser = useSelector(state => state.user.user);
 
 	const [updatedProfile, setUpdatedProfile] = useState(route.params);
@@ -19,8 +22,31 @@ const EditProfileScreen = ({route, navigation}) => {
 		setUpdatedProfile(prevData => ({...prevData, ...newData}));
 	};
 
+	const handleImagePick = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			const imageUri = result.assets[0].uri;
+			setUpdatedProfile(prevData => ({...prevData, profileImg: imageUri}));
+			return imageUri;
+		}
+		return null;
+	};
+
+	const uploadImage = async (uri) => {
+		const response = await fetch(uri);
+		const blob = await response.blob();
+		const fileRef = ref(storage, `${auth.currentUser.uid}/profileImg`);
+		await uploadBytes(fileRef, blob);
+		return await getDownloadURL(fileRef);
+	};
+
 	const handleSave = async () => {
-		console.log('updatedProfile', updatedProfile);
 		try {
 			const user = auth.currentUser;
 			if (user) {
@@ -37,9 +63,13 @@ const EditProfileScreen = ({route, navigation}) => {
 
 				if (updatedProfile.mbti) updateData.profile.mbti = updatedProfile.mbti;
 				if (updatedProfile.personality) updateData.profile.personality = updatedProfile.personality;
-				if (updatedProfile.profileImg) updateData.profileImg = updatedProfile.profileImg;
+				
+				// 프로필 이미지 업로드 및 URL 저장
+				if (updatedProfile.profileImg && updatedProfile.profileImg !== currentUser.profileImg) {
+					const imageUrl = await uploadImage(updatedProfile.profileImg);
+					updateData.profileImg = imageUrl;
+				}
 
-				console.log('Updating with data:', updateData);
 				await updateDoc(userRef, updateData);
 
 				// Redux 상태 업데이트
@@ -49,12 +79,12 @@ const EditProfileScreen = ({route, navigation}) => {
 					profile: {
 						...currentUser.profile,
 						...updateData.profile
-					}
+					},
+					profileImg: updateData.profileImg || currentUser.profileImg
 				};
 				dispatch(setUser(updatedUserData));
 
 				console.log('프로필이 성공적으로 업데이트되었습니다.');
-				console.log('Updated Redux State:', updatedUserData);
 				navigation.goBack();
 			}
 		} catch (error) {
@@ -68,8 +98,9 @@ const EditProfileScreen = ({route, navigation}) => {
 			<Header navigation={navigation} onSave={handleSave} />
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<EditForm
-						{...updatedProfile}
-						onSave={handleFormChange}
+					{...updatedProfile}
+					onSave={handleFormChange}
+					onImagePick={handleImagePick}
 				/>
 			</ScrollView>
 		</SafeAreaView>
