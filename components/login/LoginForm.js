4bf0b758eb25fe479791f5c73db5479a9860863c
comaserious <sciboviator@gmail.com>
2024-10-23 17:41:32 +0900

    [css] logo 변경

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -106,7 +106,7 @@ const LoginForm = ({ isAuthenticated, setIsAuthenticated }) => {
   return (
     <View style={styles.container}>
       <Image
-        source={require('../../assets/logo/mirrorgram-logo.png')}
+        source={require('../../assets/logo/mybot-log-color.png')}
         style={styles.logo}
       />
       <TextInput
@@ -170,7 +170,7 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: 300,
-    height: 100,
+    height: 300,
     resizeMode: 'contain',
     marginBottom: 30,
   },
